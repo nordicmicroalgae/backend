132c9bbec7d2146fecb9fb0d91c51b5342c6b7ec
Andreas Loo <andreas@gaup.io>
2023-01-25 21:15:34 +0100

    Allow serving static files from /media/ in debug

--- nordicmicroalgae/urls.py
+++ nordicmicroalgae/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -25,3 +26,11 @@ urlpatterns = [
     path('api/v1/', include('facts.urls')),
     path('api/v1/', include('openapi.urls')),
 ]
+
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
