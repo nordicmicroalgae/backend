d771a15448476480eac28dedacf3f25ceca92c14
Andreas Loo <andreas@gaup.io>
2023-01-30 14:08:36 +0100

    Restrict queryset of media in admin
    Only include media items created by the currently logged in user.
    
    If the currently logged in user is a "superuser" then this restriction
    does not apply and all media items is included in the set.

--- media/admin.py
+++ media/admin.py
@@ -40,6 +40,12 @@ class MediaAdmin(admin.ModelAdmin):
 
     list_filter = (TaxonListFilter,)
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(created_by=request.user)
+
     def save_model(self, request, obj, form, change):
         obj.created_by = request.user
         super().save_model(request, obj, form, change)
