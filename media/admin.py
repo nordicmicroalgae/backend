13aeba9c05a5aad588ed9505676d14a154b4dd33
Andreas Loo <andreas@gaup.io>
2023-01-30 12:55:20 +0100

    Add filter possibilities in admin for images
    Logged in user can filter images in the admin interface by taxon.
    
    If there is images without related taxa, the option "None" is also
    available to make it easier for the user to find images that does not
    currently belong to a taxon.

--- media/admin.py
+++ media/admin.py
@@ -4,6 +4,32 @@ from media.forms import MediaForm, ImageForm
 from media.models import Media, Image
 
 
+class TaxonListFilter(admin.SimpleListFilter):
+    title = 'taxon'
+    parameter_name = 'taxon'
+
+    def lookups(self, request, model_admin):
+        qs = model_admin.model.objects.filter(
+            created_by=request.user
+        ).distinct(
+            'taxon__scientific_name'
+        ).values_list(
+            'taxon__id',
+            'taxon__scientific_name'
+        )
+        return list(
+            map(lambda row: (str(row[0]), str(row[1])), qs)
+        )
+
+    def queryset(self, request, queryset):
+        taxon_id = self.value()
+        if taxon_id is not None:
+            if taxon_id.lower() == 'none':
+                taxon_id = None
+            return queryset.filter(taxon=taxon_id)
+        return queryset
+
+
 class MediaAdmin(admin.ModelAdmin):
     pass
 
@@ -16,6 +42,8 @@ class ImageAdmin(MediaAdmin):
         'attributes__title',
         'taxon__scientific_name',
     )
+
+    list_filter = (TaxonListFilter,)
 
 
 
