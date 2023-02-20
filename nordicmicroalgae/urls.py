38fbc006dbdb5c61d1dcba7bf9df2e4ce1bee917
Andreas Loo <andreas@gaup.io>
2023-02-20 10:37:43 +0100

    Add routes and views for admin password-reset

--- nordicmicroalgae/urls.py
+++ nordicmicroalgae/urls.py
@@ -15,10 +15,31 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 urlpatterns = [
     # Admin site routes
+    path(
+        'admin/password_reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='admin_password_reset',
+    ),
+    path(
+        'admin/password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='admin_password_reset_done',
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='admin_password_reset_confirm',
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='admin_password_reset_complete',
+    ),
     path('admin/', admin.site.urls),
     # REST API v1 routes
     path('api/v1/', include('pages.urls')),
