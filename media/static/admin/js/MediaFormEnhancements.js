57936c598072c8885133d717a1f7b99033093d7d
Andreas Loo <andreas@gaup.io>
2024-04-03 18:51:12 +0200

    Add copy-and-paste functionality for media forms
    Usable for copying content between media items,
    for example when adding multiple images that may
    share some metadata.

--- media/static/admin/js/MediaFormEnhancements.js
+++ media/static/admin/js/MediaFormEnhancements.js
@@ -7,6 +7,7 @@
     init: function() {
       MediaFormEnhancements.initFocusSearchOnOpenTaxonSelect();
       MediaFormEnhancements.initAutoUpdateTitleOnTaxonChange();
+      MediaFormEnhancements.initSaveAndRestoreFieldsTemplate();
     },
 
     initFocusSearchOnOpenTaxonSelect() {
@@ -38,7 +39,74 @@
 
         previouslySelectedName = selectedName;
       });
-    }
+    },
+
+    initSaveAndRestoreFieldsTemplate() {
+      const STORAGE_KEY = 'media.template';
+
+      const EXCLUDE_FIELDS = [
+        'taxon',
+        'file',
+        'title',
+      ];
+
+      const form = $('#content-main > form');
+
+      const fields = [
+        ...form.find(`
+          .form-row input[name],
+          .form-row select[name],
+          .form-row textarea[name]
+        `)
+      ].filter(
+        element => EXCLUDE_FIELDS
+          .includes(element.name) == false
+      );
+
+      const toolbarDOM = $(`
+        <div class="media-template-toolbar">
+          <button class="button" type="button" data-action="load">
+            Load from template
+          </button>
+          <button class="button" type="button" data-action="save">
+            Save as template
+          </button>
+        </div>
+      `);
+
+      toolbarDOM.on('click', '[data-action=load]', () => {
+        const data = JSON.parse(
+          localStorage.getItem(STORAGE_KEY) ?? '{}'
+        );
+        fields.forEach(element => {
+          if (data[element.name] != null) {
+            $(element).val(data[element.name]);
+            $(element).trigger('change');
+          }
+        });
+      });
+
+      toolbarDOM.on('click', '[data-action=save]', () => {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(
+          fields.filter(
+            element => $(element).is(':checkbox, :radio')
+              ? element.checked
+              : true
+          ).reduce(
+            (data, element) => ({
+              ...data,
+              [element.name]: $(element).is(':checkbox, :radio')
+                ? [...data[element.name] ?? [], $(element).val()]
+                : $(element).val()
+            }),
+            {}
+          )
+        ));
+      });
+
+      toolbarDOM.insertBefore(form);
+    },
+
   };
 
   $(MediaFormEnhancements.init);
