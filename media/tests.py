092035552e9891d5e15ccf9f24dd0954d71052f3
Jonatan Skogsfors <jonatan.skogsfors@smhi.se>
2025-10-06 17:05:08 +0200

    Add images of subordinate taxa
    Optionally used with query parameter 'children'.
    
    Fixes #21

--- media/tests.py
+++ media/tests.py
@@ -1,1 +1,107 @@
-# Create your tests here.
+import os
+
+import pytest
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from media.models import Media
+from taxa.models import Taxon
+
+IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
+
+
+@pytest.mark.django_db
+@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Skipped in github actions")
+def test_fetch_media_without_children(client):
+    # Given a user
+    user = get_user_model().objects.create(id=1)
+
+    # Given a parent taxon
+    parent_taxon = Taxon.objects.create(
+        id=1,
+        slug="parent-taxon",
+        children=[
+            {"slug": "child-taxon1"},
+            {"slug": "child-taxon2"},
+        ],
+    )
+
+    # Given child taxons
+    child_taxon1 = Taxon.objects.create(id=2, slug="child-taxon1")
+    child_taxon2 = Taxon.objects.create(id=3, slug="child-taxon2")
+
+    # Given an unrelated taxon
+    other_taxon = Taxon.objects.create(id=4, slug="other-taxon")
+
+    # Given media for all taxons
+    parent_media = Media.objects.create(
+        slug="parent-media", taxon=parent_taxon, created_by=user
+    )
+    Media.objects.create(slug="child-media1", taxon=child_taxon1, created_by=user)
+    Media.objects.create(slug="child-media2", taxon=child_taxon2, created_by=user)
+    Media.objects.create(slug="other-media", taxon=other_taxon, created_by=user)
+
+    # When fetching the parent taxon without requesting children
+    url = reverse("media-collection-view")
+    response = client.get(url, {"taxon": "parent-taxon"})
+    assert response.status_code == 200
+
+    # Then only the media for the parent taxon is returned
+    all_retrieved_media = response.json()["media"]
+    assert len(all_retrieved_media) == 1
+
+    retrieved_media = all_retrieved_media[0]
+    assert retrieved_media["slug"] == parent_media.slug
+
+
+@pytest.mark.django_db
+@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Skipped in github actions")
+def test_fetch_media_for_children(client):
+    # Given a user
+    user = get_user_model().objects.create(id=1)
+
+    # Given a parent taxon
+    parent_taxon = Taxon.objects.create(
+        id=1,
+        slug="parent-taxon",
+        children=[
+            {"slug": "child-taxon1"},
+            {"slug": "child-taxon2"},
+        ],
+    )
+
+    # Given child taxons
+    child_taxon1 = Taxon.objects.create(id=2, slug="child-taxon1")
+    child_taxon2 = Taxon.objects.create(id=3, slug="child-taxon2")
+
+    # Given an unrelated taxon
+    other_taxon = Taxon.objects.create(id=4, slug="other-taxon")
+
+    # Given media for all taxon
+    parent_media = Media.objects.create(
+        slug="parent-media", taxon=parent_taxon, created_by=user
+    )
+    child1_media = Media.objects.create(
+        slug="child-media1", taxon=child_taxon1, created_by=user
+    )
+    child2_media = Media.objects.create(
+        slug="child-media2", taxon=child_taxon2, created_by=user
+    )
+    Media.objects.create(slug="other-media", taxon=other_taxon, created_by=user)
+
+    # When fetching the parent taxon and requesting children
+    url = reverse("media-collection-view")
+    response = client.get(url, {"taxon": "parent-taxon", "children": "True"})
+    assert response.status_code == 200
+
+    # Then only media for the children is returned
+    all_retrieved_media = response.json()["media"]
+    assert len(all_retrieved_media) == 2
+
+    retrieved_media_slugs = set([media["slug"] for media in all_retrieved_media])
+    assert retrieved_media_slugs == {
+        child1_media.slug,
+        child2_media.slug,
+    }
+
+    assert parent_media.slug not in retrieved_media_slugs
